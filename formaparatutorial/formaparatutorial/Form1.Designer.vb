234e0b4d28d3ce3ed2703c04c0a0db400e1bc050
Alex García <lxfrnnd1988@gmail.com>
2012-11-28 07:07:47 -0600

    agregando proyecto al repo

---
+++ formaparatutorial/formaparatutorial/Form1.Designer.vb
@@ -0,0 +1,161 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.txtnombre = New System.Windows.Forms.TextBox()
+        Me.txtapellido = New System.Windows.Forms.TextBox()
+        Me.txtdireccion = New System.Windows.Forms.TextBox()
+        Me.txtcorreo = New System.Windows.Forms.TextBox()
+        Me.txttelefono = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(79, 334)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(155, 46)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Generar Documento"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'txtnombre
+        '
+        Me.txtnombre.Location = New System.Drawing.Point(79, 23)
+        Me.txtnombre.Name = "txtnombre"
+        Me.txtnombre.Size = New System.Drawing.Size(170, 20)
+        Me.txtnombre.TabIndex = 1
+        '
+        'txtapellido
+        '
+        Me.txtapellido.Location = New System.Drawing.Point(79, 84)
+        Me.txtapellido.Name = "txtapellido"
+        Me.txtapellido.Size = New System.Drawing.Size(170, 20)
+        Me.txtapellido.TabIndex = 2
+        '
+        'txtdireccion
+        '
+        Me.txtdireccion.Location = New System.Drawing.Point(79, 133)
+        Me.txtdireccion.Name = "txtdireccion"
+        Me.txtdireccion.Size = New System.Drawing.Size(170, 20)
+        Me.txtdireccion.TabIndex = 3
+        '
+        'txtcorreo
+        '
+        Me.txtcorreo.Location = New System.Drawing.Point(79, 178)
+        Me.txtcorreo.Name = "txtcorreo"
+        Me.txtcorreo.Size = New System.Drawing.Size(170, 20)
+        Me.txtcorreo.TabIndex = 4
+        '
+        'txttelefono
+        '
+        Me.txttelefono.Location = New System.Drawing.Point(79, 228)
+        Me.txttelefono.Name = "txttelefono"
+        Me.txttelefono.Size = New System.Drawing.Size(170, 20)
+        Me.txttelefono.TabIndex = 5
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(21, 26)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(42, 13)
+        Me.Label1.TabIndex = 6
+        Me.Label1.Text = "nombre"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(21, 91)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(43, 13)
+        Me.Label2.TabIndex = 7
+        Me.Label2.Text = "apellido"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(21, 140)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(50, 13)
+        Me.Label3.TabIndex = 8
+        Me.Label3.Text = "direccion"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(29, 181)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(37, 13)
+        Me.Label4.TabIndex = 9
+        Me.Label4.Text = "correo"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(26, 231)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(45, 13)
+        Me.Label5.TabIndex = 10
+        Me.Label5.Text = "telefono"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(319, 411)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.txttelefono)
+        Me.Controls.Add(Me.txtcorreo)
+        Me.Controls.Add(Me.txtdireccion)
+        Me.Controls.Add(Me.txtapellido)
+        Me.Controls.Add(Me.txtnombre)
+        Me.Controls.Add(Me.Button1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents txtnombre As System.Windows.Forms.TextBox
+    Friend WithEvents txtapellido As System.Windows.Forms.TextBox
+    Friend WithEvents txtdireccion As System.Windows.Forms.TextBox
+    Friend WithEvents txtcorreo As System.Windows.Forms.TextBox
+    Friend WithEvents txttelefono As System.Windows.Forms.TextBox
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents Label5 As System.Windows.Forms.Label
+
+End Class
---
+++ formaparatutorial/formaparatutorial/Form1.vb
@@ -0,0 +1,24 @@
+﻿Imports Microsoft.Office.Interop.Word 'control de office
+Imports System.IO 'sistema de archivos
+Imports Microsoft.Office.Interop
+
+Public Class Form1
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        Dim MSWord As New Word.Application
+        Dim Documento As Word.Document
+
+        MsgBox("El TDR se guardará en : C:\Users\afgarcia\Desktop\documento de salida.docx")
+        FileCopy("C:\plantilla video.docx", "C:\Users\afgarcia\Desktop\documento de salida.docx")
+        Documento = MSWord.Documents.Open("C:\Users\afgarcia\Desktop\documento de salida.docx")
+
+        Documento.Bookmarks.Item("nombre").Range.Text = txtnombre.Text
+        'Documento.Bookmarks.Item("apellido").Range.Text = txtapellido.Text
+        Documento.Bookmarks.Item("direccion").Range.Text = txtdireccion.Text
+        Documento.Bookmarks.Item("correo").Range.Text = txtcorreo.Text()
+        Documento.Bookmarks.Item("telefono").Range.Text = txttelefono.Text
+
+        Documento.Save()
+        MSWord.Visible = True
+    End Sub
+End Class
